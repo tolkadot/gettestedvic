0a26c5b22e7f33205e316f8b50c9d02045164d6f
tolkadot <tolkadot@gmail.com>
2021-10-01 19:36:26 +1000

    adding robots.txt & sitemap.xml

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-sass`,
-
+    {
+      resolve: `gatsby-plugin-robots-txt`,
+      options: {
+        host: `https://gettestedvictoria.com/`,
+        sitemap: `https://gettestedvictoria.com/sitemap.xml`,
+        policy: [{ userAgent: `*`, allow: `/` }],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
