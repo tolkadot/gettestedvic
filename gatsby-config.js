3a3f9cc54d8a9f4cd4c69d798ec5aaad095c62c9
tolkadot <tolkadot@gmail.com>
2021-10-19 18:51:00 +1100

    GA config

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,16 @@ module.exports = {
         policy: [{ userAgent: `*`, allow: `/` }],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-6XJJFYQ26T",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
